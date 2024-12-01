459929a13073078e1b4db70272da647a333146ff
Joanna Grzywna <joanna.grzywna@gmail.com>
2024-12-01 13:37:51 +0100

    day1 polishing code

--- puzzles/day1.py
+++ puzzles/day1.py
@@ -1,4 +1,4 @@
-def extractData(input):
+def extractData(input: list[str]) -> tuple[list[int], list[int]]:
     # print(input)
     # extract and transform data
     data1 = []
@@ -8,35 +8,35 @@ def extractData(input):
         data1.append(int(x[0]))
         data2.append(int(x[1]))
 
-    return [data1, data2]
+    return data1, data2
 
-def executePartOne(input):
-    data = extractData(input)
+def executePartOne(input: list[str]) -> None:
+    data1, data2 = extractData(input)
 
     # sort both lists asc
-    data[0].sort()
-    data[1].sort()
+    data1.sort()
+    data2.sort()
 
     # calculate absolute distance and add to sum
     sum = 0
-    for i in range(0, len(data[0])):
-        sum += abs(data[1][i] - data[0][i])
+    for i in range(0, len(data1)):
+        sum += abs(data1[i] - data2[i])
 
     print(f"Solved 1: {sum}")
 
 
-def executePartTwo(input):
-    data = extractData(input)
+def executePartTwo(input: list[str]) -> None:
+    data1, data2 = extractData(input)
 
     # calculate no of occurences in second list
     dict = {}
-    for i in data[1]:
+    for i in data2:
         dict[i] = dict[i] + 1 if i in dict else 1
     # print(dict)
 
     #calculate score for first list based on occurrences in second list
     sum = 0
-    for i in data[0]:
+    for i in data1:
         sum += (i * dict[i] if i in dict else 0)
 
     print(f"Solved 2: {sum}")
