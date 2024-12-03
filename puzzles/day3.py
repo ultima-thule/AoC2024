530b7744a166482ed8e8f8f5e560e92052cc9834
Joanna Grzywna <joanna.grzywna@gmail.com>
2024-12-03 07:33:09 +0100

    day 3 part 1 solved

--- puzzles/day3.py
+++ puzzles/day3.py
@@ -1,13 +1,43 @@
+import re
+
 def extractData(input: list[str]):
     '''Extract and transform text data'''
-    data = []
+    
+    pattern = r'mul\(([\d]+),([\d]+)\)'
+
     for l in input:
-        yield l
+        # print (f"=> line: {l}")
+        mul = re.findall(pattern, l)
+        for m in mul:
+            # print(f"found: {m} {m[0]}*{m[1]}")
+            yield m
+            
+
+def sliceData(input: list[str]):
+
+    pattern1 = r'do\(\)(.+?)don\'t\(\)'
+
+    for l in input:
+        print (f"=> line: {l}")
+        l = "do()" + l.strip() + "don't()"
+        print (f"==> line: {l}")
+        match = re.findall(pattern1, l)
+        print(match)
+
+        for t in match:
+            print (f"=> match: {t}")
+            pattern2 = r'mul\(([\d]+),([\d]+)\)'
+            mul = re.findall(pattern2, t)
+            for m in mul:
+                print(f"found: {m} {m[0]}*{m[1]}")
+                yield m
+
 
 def executePartOne(input: list[str]) -> None:
     count = 0
 
-    for l in extractData(input):
+    for m in extractData(input):
+        count += int(m[0]) * int(m[1])
         pass   
 
     print(f"Solved 1: {count}")
@@ -16,7 +46,8 @@ def executePartOne(input: list[str]) -> None:
 def executePartTwo(input: list[str]) -> None:
     count = 0
 
-    for l in extractData(input):
-        pass
+    for m in sliceData(input):
+        count += int(m[0]) * int(m[1])
+        pass   
 
     print(f"Solved 2: {count}")
