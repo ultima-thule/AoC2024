c710db2624757ca7b3906469b952309fc7542f29
ultima-thule <joanna.grzywna@gmail.com>
2024-12-05 22:53:25 +0100

    day 5 refactor

--- puzzles/day5.py
+++ puzzles/day5.py
@@ -1,138 +1,91 @@
 def extract_data(input: list[str]):
-    '''Extract and transform text data'''
-    data_order = set()
-    data_update = []
-    data_dict: dict[str,list[str]] = {}
+    '''Extracts and transforms input text data'''
+    ordering_rules = set()
+    page_numbers: list[list[str]] = []
+    ordering_rules_aggr: dict[str,list[str]] = {}
 
-    start_update = False
+    start_page_numbers = False
 
     for line in input:
         line = line.strip()
+        # section with page numbers starts
         if line == "":
-            start_update = True
+            start_page_numbers = True
             continue
-        if start_update:
-            data_update.append(line.split(","))
-        else:
-            data_order.add(line)
-            pair = line.split("|")
-            if pair[0] not in data_dict:
-                # data_dict[pair[0]] = set()
-                data_dict[pair[0]] = []
-            # data_dict[pair[0]].add(pair[1])
-            data_dict[pair[0]].append(pair[1])
 
-    return data_order, data_update, data_dict
+        if start_page_numbers:
+            # turn each update into list of pages
+            page_numbers.append(line.split(","))
+        else: 
+            # save ordering rules in a list of pages
+            ordering_rules.add(line)
             
-def generate_orders(update: list[str]):
-    result = set()
+            # save ordering rules as dict of pages following the key page
+            pair = line.split("|")
+            if pair[0] not in ordering_rules_aggr:
+                ordering_rules_aggr[pair[0]] = []
+            ordering_rules_aggr[pair[0]].append(pair[1])
 
-    for i in range (0, len(update)):
-        for j in range (i+1, len(update)):
-            result.add(update[i] + "|" + update[j])
+    return ordering_rules, page_numbers, ordering_rules_aggr
+            
+def generate_ordering_rules(pages: list[str]) -> set:
+    '''Generates all possible ordering rules for a given list of pages'''
+    all_rules = set()
 
-    # print(f"Generated set: {control}")
-    return result
+    for i in range (0, len(pages)):
+        for j in range (i+1, len(pages)):
+            all_rules.add(pages[i] + "|" + pages[j])
 
-def check_order(order, data_order):
-    result = order & data_order
-    # print(f"Intersection: {result} len of {len(result)} compared to {len(order)}")
-    return len(result) == len(order)
+    return all_rules
 
-def select_middle(update):
-    index = int((len(update) - 1) / 2)
-    # print(f"Middle element of {update} is {update[index]}")
-    return int(update[index])
+def check_rules(current_rules: set, all_ordering_rules: set, missing_only: bool) -> bool:
+    '''Checks whether ordering rules for current update are present (or not, based on missing_only variable) in all rules set'''
+    result = current_rules & all_ordering_rules
+    all_present = (len(result) == len(current_rules))
+    return all_present is False if missing_only else all_present is True
 
-def fix_order(update, data_order):
-    return update
-
-
-def execute_part_one(input: list[str]) -> None:
-    count = 0
-
-    data_order, data_update, data_dict = extract_data(input)
-    
-    # print(data_order)
-    # print(data_update)
-
-    for du in data_update:
-        # print(f"\n===>>>> Checking update: {du}")
-        order = generate_orders(du)
-        if check_order(order, data_order):
-            count += select_middle(du)
-
-    print(f"Solved 1: {count}")
-
-
-def build_nodes_dict(order, full_dict):
-    result = full_dict.copy()
-
-    for k in list(result.keys()):
-        # print(f"Result {k} of value {result[k]} in order: {k in order}")
-        if k not in order:
-            del result[k]
-
-    # print(f"Final result: {result}")
-
-    for k in list(result.keys()):
-        # print(f"i: {k}, j: {result[k]}")
-        lista = []
-        for item in result[k]:
-            # print(f"item {item} in order? {item in order}")
-            if item in order:
-                lista.append(item)
-        result[k] = lista
-    #    result[i][:] = [x for x in i if x in order]
-
-    print (f"Modified dict: {result}")
-    return result
-
-def calculate_middle(nodes_dict, lenght):
-    for k in list(nodes_dict.keys()):
-        print(f"Item: {k}")
-        if len(nodes_dict[k]) == lenght:
-            print(f"Item found! {k}")
+def select_middle_page(rules_subset: dict[str,list[str]], no_of_followers: int) -> int:
+    '''Selects page in the middle of ordering rules subset based on number of pages that follow it'''
+    for k in rules_subset:
+        if len(rules_subset[k]) == no_of_followers:
             return int(k)
     return 0
 
+def build_ordering_rules_subset(pages_update: list[str], ordering_rules_all: dict[str,list[str]]) -> dict[str,list[str]]:
+    '''Selects only those ordering rules which consist of pages included in a single update '''
+    ordering_rules_subset = ordering_rules_all.copy()
+
+    for k in list(ordering_rules_subset.keys()):
+        if k not in pages_update:
+            del ordering_rules_subset[k]
+
+    for k in ordering_rules_subset:
+        subset = []
+        for item in ordering_rules_subset[k]:
+            if item in pages_update:
+                subset.append(item)
+        ordering_rules_subset[k] = subset
+
+    return ordering_rules_subset
+
+
+def calculate_pages_sum(input: list[str], fix_invalid: bool) -> int:
+    '''Calculates the sum of pages number which are valid (scenario 1) or needs fixing (scenario 2)'''
+    page_numbers_sum = 0
+    all_ordering_rules, pages_update, ordering_rules_aggr = extract_data(input)
+    
+    for pu in pages_update:
+        current_rules = generate_ordering_rules(pu)
+        if check_rules(current_rules, all_ordering_rules, fix_invalid):
+            rules_subset = build_ordering_rules_subset(pu, ordering_rules_aggr)
+
+            no_of_followers = int((len(pu) - 1) / 2)
+            page_numbers_sum += select_middle_page(rules_subset, no_of_followers)
+
+    return page_numbers_sum
+
+def execute_part_one(input: list[str]) -> None:
+    print(f"Solved 1: {calculate_pages_sum(input, False)}\n")
 
 def execute_part_two(input: list[str]) -> None:
-    count = 0
-    data_order, data_update, data_dict = extract_data(input)
-    
-    # print(data_order)
-    # print(data_update)
-    print(f"data_dict: {data_dict}")
-
-    orders_to_fix = []
-
-    for du in data_update:
-        # print(f"\n===>>>> Checking update: {du}")
-        order = generate_orders(du)
-        if not check_order(order, data_order):
-            orders_to_fix.append(du)
-
-    print(f"Orders to fix: {orders_to_fix}")
-
-    for o in orders_to_fix:
-        # transform order from list to set
-        to_set = set(o)
-        print(f"\nFixing order: {to_set}")
-
-        #create empty dictionary of pairs
-        order_nodes = build_nodes_dict(o, data_dict)
-        # print(f"Order nodes: {order_nodes}")
-
-        mid_len = int((len(o) - 1) / 2)
-        print(f"Searching {o} for len: {mid_len}")
-        count += calculate_middle(order_nodes, mid_len)
-
-        # #iterate over all numbers in set
-        # for i in to_set:
-        #     # if number exists in 
-        #     if i in data_dict:
-        #         for e in data_dict[i]:
-
-
-    print(f"Solved 2: {count}")
+    print(f"Solved 2: {calculate_pages_sum(input, True)}\n")
