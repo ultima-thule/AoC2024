e9a85dfbdeab56b65cdb192aecf5444c01bb1d8e
ultima-thule <joanna.grzywna@gmail.com>
2024-12-06 12:35:48 +0100

    added execution time

--- aoc2024.py
+++ aoc2024.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 def read_input(day):
     with open(f"puzzles/data/day{day}.txt", "r+") as dataFile:
@@ -22,8 +23,12 @@ def run_part(day, part, input):
 def main(argv):
     input = read_input(argv[0])
     
+    start_time = time.time()
     run_part(argv[0], 1, input)
+    print("--- %s seconds ---\n" % (time.time() - start_time))
+    start_time = time.time()
     run_part(argv[0], 2, input)
+    print("--- %s seconds ---\n" % (time.time() - start_time))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
