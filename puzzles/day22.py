cabf76939543bc9c029e4aa1d53d87c34ce95856
ultima-thule <joanna.grzywna@gmail.com>
2024-12-24 10:17:13 +0100

    day 22 part2 solved

--- puzzles/day22.py
+++ puzzles/day22.py
@@ -1,4 +1,4 @@
-import math
+from collections import defaultdict
 
 def extract_data(input: list[str]):
     '''Extract and transform text data'''
@@ -6,70 +6,72 @@ def extract_data(input: list[str]):
     for line in input:
         data.append(int(line))
 
-    # print(f"Data: {data}")
-
     return data
 
-def mix(num, secret):
-    res = num ^ secret
-    return res
+def step(secret):
+    tmp = secret << 6
+    secret ^= tmp
+    secret &= (2 << 23) - 1
 
-def prune(secret):
-    res = secret % 16777216
-    return res
+    tmp = secret >> 5
+    secret ^= tmp
+    secret &= (2 << 23) - 1
 
-def step_1(secret):
-    # print(f"Step 1 Secret: {secret}")
-    tmp1 = secret * 64
-    tmp2 = mix(tmp1, secret)
-    tmp3 = prune(tmp2)
+    tmp = secret * 2048
+    secret ^= tmp
+    secret &= (2 << 23) - 1
 
-    return tmp3
-
-def step_2(secret):
-    # print(f"Step 2 Secret: {secret}")
-    tmp1 = secret / 32
-    tmp2 = math.floor(tmp1)
-    tmp3 = mix(tmp2, secret)
-    tmp4 = prune(tmp3)
-
-    return tmp4
-
-def step_3(secret):
-    # print(f"Step 3 Secret: {secret}")
-    tmp1 = secret * 2048
-    tmp2 = mix(tmp1, secret)
-    tmp3 = prune(tmp2)
-
-    return tmp3
+    return secret
 
 def simulate(num, iters_no):
     curr_secret = num
-    for i in range(0, iters_no):
-        curr_secret = step_1(curr_secret)
-        curr_secret = step_2(curr_secret)
-        curr_secret = step_3(curr_secret)
-        # print(f"Iteration {i} result {curr_secret}")
-    return curr_secret
+
+    d_deltas = [0] * iters_no
+    for i in range(iters_no):
+        next_sec = step(curr_secret)
+        d_deltas[i] = next_sec % 10 - curr_secret % 10
+        curr_secret = next_sec
+
+    return curr_secret, d_deltas
 
 def execute_part_one(input: list[str]) -> None:
     count = 0
 
     data = extract_data(input)
-       
-    for d in data:
-        nr = simulate(d, 2000)
-        count += nr
 
-    # print(f"Tests: prune {prune(100000000)}")
-    # print(f"Tests: mix {mix(15, 42)}")
+    for d in data:
+        last_secret, _ = simulate(d, 2000)
+        count += last_secret
 
     print(f"Solved 1: {count}")
 
 
 def execute_part_two(input: list[str]) -> None:
-    count = 0
-
     data = extract_data(input)
 
-    print(f"Solved 2: {count}")
+    price_deltas = []
+    loop_len = 2000
+
+    for d in data:
+        _, deltas = simulate(d, loop_len)
+        price_deltas.append(deltas)
+
+    price_sums = defaultdict(int)
+    for start_sec, delta in zip(data, price_deltas):
+        cumulative = {}
+        d0, d1, d2 = delta[0:3]
+        curr_sum = start_sec % 10 + d0 + d1 + d2
+        # print(f"Sum: {curr_sum}")
+        for d3 in delta[3:loop_len]:
+            # save cumulative sum
+            curr_sum += d3
+            cumulative.setdefault((d0, d1, d2, d3), curr_sum)
+            # move deltas window by 1
+            d0, d1, d2 = d1, d2, d3
+
+        # add sum to dictonary of sequences, we will be maximizing that
+        for k, v in cumulative.items():
+            price_sums[k] += v
+
+    # max sum is the solution
+    print(f"Solved 2: {max(price_sums.values())}")
