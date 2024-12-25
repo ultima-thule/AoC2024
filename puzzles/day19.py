4269259fd6de9d68fd9d2df3ec9ca974a3f6ca86
Joanna Grzywna <joanna.grzywna@gmail.com>
2024-12-25 22:15:25 +0100

    day 19 part 2 solved

--- puzzles/day19.py
+++ puzzles/day19.py
@@ -4,7 +4,6 @@ import functools
 def extract_data(input: list[str]):
     words = []
     grammar = defaultdict(defaultdict)
-    max_len = 0
     
     for i in range(0, len(input)):
         line = input[i].strip()
@@ -13,43 +12,27 @@ def extract_data(input: list[str]):
         elif len(line) > 0:
             words.append(line)
 
-    # print(f"Grammar: {grammar}")
-    # print(f"Words: {words}")
-    # print(f"Max len: {max_len}")
-
-    return grammar, words, max_len
+    return grammar, words
 
 def parse_grammar(line, grammar):
-    max_len = 0
     spl = line.split(", ")
     for i in spl:
         l = len(i)
-        max_len = max(max_len, l)
         if l not in grammar:
             grammar[l] = {}
         grammar[l][i] = True
-    return max_len
 
 def execute_part_one(input: list[str]) -> None:
     count = 0
 
-    grammar, words, max_len = extract_data(input)
-
-    results = defaultdict(bool)
-    saved = {}
+    grammar, words = extract_data(input)
 
     @functools.cache
     def is_valid(word) -> bool:
         return word in grammar[len(word)] or any(is_valid(word[0:i]) and is_valid(word[i:]) for i in range(1, len(word)))
 
     for w in words:
-        if is_valid(w):
-            count += 1
-        # print(f"\n => Checking word {w}: {ret}")
-
-    # print(f"Results size: {len(results)}")
-    # print(f"Results: {results}")
-    # print(f"Saved: {saved}")
+        count += 1 if is_valid(w) else 0
 
     print(f"Solved 1: {count}")
 
@@ -57,6 +40,13 @@ def execute_part_one(input: list[str]) -> None:
 def execute_part_two(input: list[str]) -> None:
     count = 0
 
-    grammar, words, max_len = extract_data(input)
+    grammar, words = extract_data(input)
+
+    @functools.cache
+    def count_possibilities(word) -> int:
+        return (word in grammar[len(word)]) + sum(count_possibilities(word[i:]) for i in range(1, len(word)) if word[0:i] in grammar[len( word[0:i])])
+
+    for w in words:
+        count += count_possibilities(w)
 
     print(f"Solved 2: {count}")
